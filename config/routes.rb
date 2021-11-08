16dcad7a9bcb8d73066b6cf4d07853de30f471c5
Eric Yi <erickanginyi@gmail.com>
2021-11-08 14:15:42 -0500

    commit tree ffa2d6f77a209fb109f49f3f1d1e6b38dcd280bc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+  # This custom route shows a single movie summary
+  get "/movies/:id/summary", to: "movies#summary"
+
+  # This custom route shows ALL movie summaries
+  get "/movie_summaries", to: "movies#summaries"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
